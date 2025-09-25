07b180e60e79dfeaf0a7dc41b7ac53fac8dfa227
Janne Mareike Koschinski <janne@kuschku.de>
2025-09-25 18:58:32 +0200

    resolve eslint/prettier findings

--- apps/web/src/components/views/spaces/QuickSettingsButton.tsx
+++ apps/web/src/components/views/spaces/QuickSettingsButton.tsx
@@ -98,10 +98,7 @@ const QuickSettingsButton: React.FC<{
                 <StyledCheckbox
                     className="mx_QuickSettingsButton_option"
                     checked={!!favouritesEnabled}
-                    onChange={onMetaSpaceChangeFactory(
-                        MetaSpace.Favourites,
-                        "WebQuickSettingsPinToSidebarCheckbox",
-                    )}
+                    onChange={onMetaSpaceChangeFactory(MetaSpace.Favourites, "WebQuickSettingsPinToSidebarCheckbox")}
                 >
                     <FavouriteSolidIcon className="mx_QuickSettingsButton_icon" />
                     {_t("common|favourites")}
@@ -109,10 +106,7 @@ const QuickSettingsButton: React.FC<{
                 <StyledCheckbox
                     className="mx_QuickSettingsButton_option"
                     checked={!!peopleEnabled}
-                    onChange={onMetaSpaceChangeFactory(
-                        MetaSpace.People,
-                        "WebQuickSettingsPinToSidebarCheckbox",
-                    )}
+                    onChange={onMetaSpaceChangeFactory(MetaSpace.People, "WebQuickSettingsPinToSidebarCheckbox")}
                 >
                     <UserProfileSolidIcon className="mx_QuickSettingsButton_icon" />
                     {_t("common|people")}
