3af3203a84774ce4f59080ef6096b75300fafa00
Janne Mareike Koschinski <janne@kuschku.de>
2026-03-11 18:55:52 +0100

    Add custom sorting for andy

--- apps/web/src/stores/room-list-v3/skip-list/SectionProcessor.ts
+++ apps/web/src/stores/room-list-v3/skip-list/SectionProcessor.ts
@@ -6,75 +6,98 @@ Please see LICENSE files in the repository root for full details.
 */
 
 import { type Room } from "matrix-js-sdk/src/matrix";
-
-import { type RoomListEntry } from "../RoomListStoreV3.ts";
-import { type Filter } from "./filters";
-import { FavouriteFilter } from "./filters/FavouriteFilter.ts";
-import { InvitesFilter } from "./filters/InvitesFilter.ts";
-import { MentionsFilter } from "./filters/MentionsFilter.ts";
-import { UnreadFilter } from "./filters/UnreadFilter.ts";
-import { LowPriorityFilter } from "./filters/LowPriorityFilter.ts";
 import { RoomListSectionKey } from "@element-hq/web-shared-components";
 
+import { type RoomListEntry } from "../RoomListStoreV3.ts";
+import DMRoomMap from "../../../utils/DMRoomMap.ts";
+import { RoomNotificationStateStore } from "../../notifications/RoomNotificationStateStore.ts";
+import { getMarkedUnreadState } from "../../../utils/notifications.ts";
+import { DefaultTagID } from "./tag.ts";
+
 const enum RoomListSubsection {
-    Favourite,
-    Mention,
-    Unread,
-    Invite,
-    LowPriority,
-    Chat,
+    UnreadDirect,
+    UnreadFavourite,
+    UnreadNormal,
+    UnreadLowPriority,
+    Other,
 }
 
-const filters: { [key in RoomListSubsection]: Filter | null } = {
-    [RoomListSubsection.Favourite]: new FavouriteFilter(),
-    [RoomListSubsection.Invite]: new InvitesFilter(),
-    [RoomListSubsection.Mention]: new MentionsFilter(),
-    [RoomListSubsection.Unread]: new UnreadFilter(),
-    [RoomListSubsection.LowPriority]: new LowPriorityFilter(),
-    [RoomListSubsection.Chat]: null,
+const filters: { [key in RoomListSubsection]: (room: Room) => boolean } = {
+    [RoomListSubsection.UnreadDirect]: (room: Room) => {
+        const isUnread = RoomNotificationStateStore.instance.getRoomState(room).hasUnreadCount || !!getMarkedUnreadState(room);
+        const isDirect = !!DMRoomMap.shared().getUserIdForRoomId(room.roomId);
+        const isMention = RoomNotificationStateStore.instance.getRoomState(room).isMention;
+        return isMention || (isDirect && isUnread);
+    },
+    [RoomListSubsection.UnreadFavourite]: (room: Room) => {
+        const isUnread = RoomNotificationStateStore.instance.getRoomState(room).hasUnreadCount || !!getMarkedUnreadState(room);
+        const isFavourite = !!room.tags[DefaultTagID.Favourite];
+        return isUnread && isFavourite;
+    },
+    [RoomListSubsection.UnreadLowPriority]: (room: Room) => {
+        const isUnread = RoomNotificationStateStore.instance.getRoomState(room).hasUnreadCount || !!getMarkedUnreadState(room);
+        const isLowPriority = !!room.tags[DefaultTagID.LowPriority];
+        return isUnread && isLowPriority;
+    },
+    [RoomListSubsection.UnreadNormal]: (room: Room) => {
+        return RoomNotificationStateStore.instance.getRoomState(room).hasUnreadCount || !!getMarkedUnreadState(room);
+    },
+    [RoomListSubsection.Other]: (room: Room) => {
+        return true;
+    },
 };
 
 const categories = [
-    RoomListSubsection.Favourite,
-    RoomListSubsection.Invite,
-    RoomListSubsection.Mention,
-    RoomListSubsection.Unread,
-    RoomListSubsection.LowPriority,
-    RoomListSubsection.Chat,
+    RoomListSubsection.UnreadDirect,
+    RoomListSubsection.UnreadFavourite,
+    RoomListSubsection.UnreadLowPriority,
+    RoomListSubsection.UnreadNormal,
+    RoomListSubsection.Other,
 ];
 
 export class RoomListSectionHeader {
     public constructor(public readonly key: RoomListSectionKey) {}
 }
 
-const sections: { header: RoomListSectionHeader; subsections: RoomListSubsection[] }[] = [
-    { header: new RoomListSectionHeader(RoomListSectionKey.Favourite), subsections: [RoomListSubsection.Favourite] },
+const sections: {
+    header: RoomListSectionHeader; subsections: RoomListSubsection[]
+}[] = [
+    {
+        header: new RoomListSectionHeader(RoomListSectionKey.People),
+        subsections: [RoomListSubsection.UnreadDirect]
+    },
+    {
+        header: new RoomListSectionHeader(RoomListSectionKey.Favourite),
+        subsections: [RoomListSubsection.UnreadFavourite],
+    },
     {
         header: new RoomListSectionHeader(RoomListSectionKey.Unread),
-        subsections: [RoomListSubsection.Invite, RoomListSubsection.Mention, RoomListSubsection.Unread],
+        subsections: [RoomListSubsection.UnreadNormal]
     },
-    { header: new RoomListSectionHeader(RoomListSectionKey.Chat), subsections: [RoomListSubsection.Chat] },
     {
         header: new RoomListSectionHeader(RoomListSectionKey.LowPriority),
-        subsections: [RoomListSubsection.LowPriority],
+        subsections: [RoomListSubsection.UnreadLowPriority],
+    },
+    {
+        header: new RoomListSectionHeader(RoomListSectionKey.Chat),
+        subsections: [RoomListSubsection.Other],
     },
 ];
 
 export class SectionProcessor {
     public process(rooms: Iterable<Room>): RoomListEntry[] {
         const groupedRooms: { [key in RoomListSubsection]: Room[] } = {
-            [RoomListSubsection.Favourite]: [],
-            [RoomListSubsection.Invite]: [],
-            [RoomListSubsection.Mention]: [],
-            [RoomListSubsection.Unread]: [],
-            [RoomListSubsection.LowPriority]: [],
-            [RoomListSubsection.Chat]: [],
+            [RoomListSubsection.UnreadDirect]: [],
+            [RoomListSubsection.UnreadFavourite]: [],
+            [RoomListSubsection.UnreadLowPriority]: [],
+            [RoomListSubsection.UnreadNormal]: [],
+            [RoomListSubsection.Other]: [],
         };
 
         for (const room of rooms) {
             for (const category of categories) {
                 const filter = filters[category];
-                if (filter == null || filter.matches(room)) {
+                if (filter == null || filter(room)) {
                     groupedRooms[category].push(room);
                     break;
                 }
